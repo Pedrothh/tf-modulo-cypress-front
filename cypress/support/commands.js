250a16c57bf526e3b713f8ced0dc84d105f7fcdb
Pedro Thomé <pedro.th.dev@gmail.com>
2022-11-22 20:37:16 -0300

    :tada: Adicionando estrutura base do projeto -> Cypress + Allure + Cucumber

---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+beforeEach(() => {
+    Given("que estou na página de login", () => {
+        cy.visit("https://itera-qa.azurewebsites.net/Login")
+    })
+});
+
+
+// import MainPage from "../integration/pages/MainPage";
+// const mainPage = new MainPage;
+
+// afterEach(() => {
+//     Then("limpo a massa de dados", () => {
+//         mainPage.deleteUser();
+//     })
+// })
